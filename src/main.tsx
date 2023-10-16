b220372c1423532805a63b4a8ecd3aa02ad51786
Joram3000 <joramkroon@live.nl>
2023-10-16 18:37:00 +0200

    remove unused css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./pages/Error";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
