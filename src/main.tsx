04b0ff87c95a10d8c9a3a461942fda39aa1e1730
Joram3000 <joramkroon@live.nl>
2023-10-16 18:12:34 +0200

    add react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./pages/Error";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  { path: "videoplayer", element: <VideoPlayer /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
