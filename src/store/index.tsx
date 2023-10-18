e146f0d295a032bb93b885ffdc30dab575530cec
Joram3000 <joramkroon@live.nl>
2023-10-18 22:22:08 +0200

    cleanup

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,12 +1,6 @@
-import { createStore, applyMiddleware, compose } from "redux";
-
-import ReduxThunk from "redux-thunk";
+import { createStore } from "redux";
 import reducer from "./rootReducer";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const enhancer = composeEnhancers(applyMiddleware(ReduxThunk));
-
-const store = createStore(reducer, enhancer);
+const store = createStore(reducer);
 
 export default store;
