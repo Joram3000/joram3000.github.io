16e9107fa534e6f14a3540c197bf54ff8f7cb162
Joram3000 <joramkroon@live.nl>
2023-10-18 11:49:29 +0200

    remove P5

--- src/store/seqState/actions.ts
+++ src/store/seqState/actions.ts
@@ -2,9 +2,3 @@ export const PatternUpdater = (Payload: number[][]) => ({
   type: "PATTERNUPDATER",
   payload: Payload,
 });
-
-// TRANSPORT UPDATER
-// export const Transportupdater = (Payload: string) => ({
-//   type: "TRANSPORTUPDATER",
-//   payload: Payload,
-// });
