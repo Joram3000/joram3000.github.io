f2da007ee4d90a75b1de0feaea406c8e9ef469d4
Joram3000 <joramkroon@live.nl>
2023-10-20 14:49:58 +0200

    create menuData to keep it DRY

---
+++ src/routes/menuData.ts
@@ -0,0 +1,7 @@
+export const menuData = [
+  { link: "home", label: "Home" },
+  { link: "patternmaker", label: "Patternmaker" },
+  { link: "videoplayer", label: "Videoplayer" },
+  { link: "about", label: "About" },
+  { link: "contact", label: "Contact" },
+];
