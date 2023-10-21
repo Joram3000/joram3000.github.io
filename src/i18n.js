5d8df7a0dfb805054862fd052ff9c254c919c312
Joram3000 <joramkroon@live.nl>
2023-10-21 15:07:29 +0200

    add i18 hello world

--- src/i18n.js
+++ src/i18n.js
@@ -8,21 +8,13 @@ const resources = {
   en: {
     translation: {
       "Welcome to React": "Welcome to React and react-i18next",
+      "Welcome to React2": "This is the English Sentence",
     },
   },
   nl: {
     translation: {
-      "Welcome to React": "welkom bij de REACTS",
-    },
-  },
-  fr: {
-    translation: {
       "Welcome to React": "Bienvenue à React et react-i18next",
-    },
-  },
-  ar: {
-    translation: {
-      "Welcome to React": "hakbar ravigot",
+      "Welcome to React2": "Welkom in Nederland het godenland",
     },
   },
 };
@@ -31,7 +23,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "nl", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
