419f5525b7ed54c411687e7a4b646cbbc52f3988
Joram3000 <joramkroon@live.nl>
2023-10-28 18:32:13 +0200

    superbasic routing

--- src/pages/beatbattle-page/BeatBattlePage.tsx
+++ src/pages/beatbattle-page/BeatBattlePage.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { getBeatStateSelector } from "../../store/beatbattle/selectors";
-import { Container, Group, Title, Text, Button } from "@mantine/core";
+import { Container, Group, Title, Text } from "@mantine/core";
 import { Link } from "react-router-dom";
 
 const BeatBattlePage: React.FC = () => {
@@ -13,12 +13,13 @@ const BeatBattlePage: React.FC = () => {
       <Title>Home page voor de beat batttle</Title>
 
       {beatBattleState.contests.map((contest, i) => (
-        <Group key={i} onClick={() => onClick(i)}>
-          <Link to={`/beatbattle/${i}`}>
+        <Link to={`/beatbattle/${i}`}>
+          <Group key={i}>
             <Text>{contest.sample.name}</Text>
+
             <Text>{contest.sample.dateAdded}</Text>
-          </Link>
-        </Group>
+          </Group>
+        </Link>
       ))}
     </Container>
   );
