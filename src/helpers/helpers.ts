887c15e72f7256f3a6d8f833987448e8e38a5a86
Joram3000 <joramkroon@live.nl>
2023-10-28 11:56:58 +0200

    create random color helper

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -15,3 +15,12 @@ export const truncateHttps = (string: string) => {
   const regex = /^https?:\/\/?/i;
   return string.replace(regex, "");
 };
+
+export const randomColor = (opacity: number = 0.5) => {
+  const random = (min: number, max: number) =>
+    Math.random() * (max - min) + min;
+  return `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(
+    0,
+    255
+  )}, ${opacity})`;
+};
