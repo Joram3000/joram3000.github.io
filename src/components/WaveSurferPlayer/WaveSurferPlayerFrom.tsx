868e32630aa43c50d9838f317e8b1363d804425f
Joram3000 <joramkroon@live.nl>
2023-10-31 09:14:31 +0100

    create simple waeplayer

--- src/components/WaveSurferPlayer/WaveSurferPlayerFrom.tsx
+++ src/components/WaveSurferPlayer/WaveSurferPlayerFrom.tsx
@@ -1,86 +1,66 @@
-import { Button, Checkbox, Group, Stack, Text } from "@mantine/core";
-import { useState, useEffect, useRef, useCallback, RefObject } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  RefObject,
+} from "react";
 import WaveSurfer, { WaveSurferOptions } from "wavesurfer.js";
 import TimelinePlugin from "wavesurfer.js/dist/plugins/timeline";
-import HoverPLugin from "wavesurfer.js/dist/plugins/hover";
-import RegionsPlugin from "wavesurfer.js/dist/plugins/regions.esm.js";
-import { IconZoomIn, IconZoomOut } from "@tabler/icons-react";
-import { randomColor } from "../../helpers/helpers";
+import HoverPlugin from "wavesurfer.js/dist/plugins/hover";
+import {
+  ActionIcon,
+  Button,
+  Container,
+  Flex,
+  Group,
+  Slider,
+  Stack,
+  Text,
+  useMantineTheme,
+  rem,
+} from "@mantine/core";
+import {
+  IconZoomIn,
+  IconZoomOut,
+  IconRepeat,
+  IconPlayerPlayFilled,
+  IconPlayerPauseFilled,
+  IconSquareRoundedChevronRight,
+  IconCircle,
+} from "@tabler/icons-react";
+import PitchSlider from "./pitch-slider";
+import RegionsPlugin from "wavesurfer.js/dist/plugins/regions";
+import Region from "wavesurfer.js";
+import { useDisclosure } from "@mantine/hooks";
 
-// WaveSurfer hook
 const useWavesurfer = (
   containerRef: RefObject<HTMLDivElement>,
   options: WaveSurferOptions
-) => {
-  // @ts-ignore
-  const [wavesurfer, setWavesurfer] = useState<WaveSurfer>(null);
+): WaveSurfer | null => {
+  const [wavesurfer, setWavesurfer] = useState<WaveSurfer | null>(null);
 
   useEffect(() => {
     if (!containerRef.current) return;
-
     const ws = WaveSurfer.create({
       ...options,
       container: containerRef.current,
     });
     ws.registerPlugin(
-      HoverPLugin.create({
+      HoverPlugin.create({
         lineColor: "#ff0000",
-        lineWidth: 1,
+        lineWidth: 2,
         labelBackground: "#555",
         labelColor: "#fff",
-        labelSize: "11px",
+        labelSize: "16px",
       })
     );
-
-    ws.registerPlugin(TimelinePlugin.create());
-    const wsRegions = ws.registerPlugin(RegionsPlugin.create());
-
-    // Create some regions at specific time ranges
-    ws.on("decode", () => {
-      // Regions
-      wsRegions.addRegion({
-        start: 1,
-        end: 3,
-        loop: true,
-        content: "Kadaver",
-        color: randomColor(),
-        drag: false,
-        resize: true,
-      });
-      wsRegions.addRegion({
-        start: 9,
-        end: 10,
-        loop: true,
-        content: "Cramped region",
-        color: randomColor(),
-        minLength: 1,
-        maxLength: 10,
-      });
-    });
-
-    wsRegions.enableDragSelection({
-      color: "rgba(255, 0, 0, 0.1)",
-    });
-
-    wsRegions.on("region-updated", (region: any) => {
-      console.log("Updated region", region);
-    });
-
-    const loop = true; // was eerst LET
-    let activeRegion: any = null;
-    wsRegions.on("region-in", (region: any) => {
-      activeRegion = region;
-    });
-
-    wsRegions.on("region-out", (region: any) => {
-      if (activeRegion === region) {
-        if (loop) {
-          region.play();
-        } else {
-          activeRegion = null;
-        }
-      }
-    });
+    ws.registerPlugin(
+      TimelinePlugin.create({
+        height: 40,
+        insertPosition: "beforebegin",
+      })
+    );
 
     setWavesurfer(ws);
 
@@ -92,79 +72,177 @@ const useWavesurfer = (
   return wavesurfer;
 };
 
-// Create a React component that will render wavesurfer.
-// Props are wavesurfer options.
-export const WaveSurferPlayert = (props: WaveSurferOptions) => {
+const WaveSurferPlayer: React.FC<WaveSurferOptions> = (props) => {
+  const theme = useMantineTheme();
   const containerRef: RefObject<HTMLDivElement> =
     useRef() as RefObject<HTMLDivElement>;
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
-  // const [minPxPerSec, setMinPxPerSec] = useState(60);
+  const [activeRegion, setActiveRegion] = useState<Region | null>(null);
+  const [savedRegions, setSavedRegions] = useState<Region[]>([]);
+  const [cuePoint, setCuePoint] = useState<Region>();
+  const [zoom, setZoom] = useState(160);
+  const [follow, { toggle: toggleFollow }] = useDisclosure(false);
+  const [loop, { toggle: toggleLoop }] = useDisclosure(false);
   const wavesurfer = useWavesurfer(containerRef, props);
+  const wsRegions = wavesurfer?.registerPlugin(RegionsPlugin.create());
 
-  const onPlayClick = useCallback(() => {
-    wavesurfer.isPlaying() ? wavesurfer.pause() : wavesurfer.play();
-  }, [wavesurfer]);
+  useEffect(() => {
+    wavesurfer?.on("ready", () => {
+      wsRegions.enableDragSelection({
+        color: "rgba(255, 0, 0, 0.2)",
+      });
+      setCuePoint(
+        wsRegions.addRegion({
+          id: "start",
+          start: 0.05,
+          content: "Marker",
+          color: "red",
+        })
+      );
+
+      wsRegions.on("region-in", (region: Region) => setActiveRegion(region));
+
+      wsRegions.on("region-out", (region: Region) => {
+        console.log("kom ik hier wel?", loop);
+        loop ? region.play() : setActiveRegion(null);
+      });
+
+      wsRegions.on("region-created", () => {
+        console.log("creatieerd", wsRegions);
+        setSavedRegions(wsRegions.regions);
+      });
+
+      wsRegions.on("region-clicked", (region: Region, e: MouseEvent) => {
+        e.stopPropagation();
+        setActiveRegion(region);
+        region.play();
+      });
+    });
+  }, [wsRegions]);
+
+  const playRegion = (region: Region) => {
+    setActiveRegion(region);
+    region.play();
+  };
 
   useEffect(() => {
     if (!wavesurfer) return;
     setCurrentTime(0);
     setIsPlaying(false);
-    // setMinPxPerSec(60);
+
     const subscriptions = [
       wavesurfer.on("play", () => setIsPlaying(true)),
       wavesurfer.on("pause", () => setIsPlaying(false)),
       wavesurfer.on("timeupdate", (currentTime) => setCurrentTime(currentTime)),
-      // wavesurfer.setPlaybackRate(0.8, false),
     ];
+
     return () => {
       subscriptions.forEach((unsub) => unsub());
     };
   }, [wavesurfer]);
 
-  // const handleZoom = useCallback(
-  //   (newMinPxPerSec: number) => {
-  //     setMinPxPerSec(newMinPxPerSec);
-  //     wavesurfer.zoom(newMinPxPerSec);
-  //   },
-  //   [wavesurfer]
-  // );
+  const onPlayClick = useCallback(() => {
+    if (wavesurfer) {
+      wavesurfer.isPlaying() ? wavesurfer.pause() : wavesurfer.play();
+    }
+  }, [wavesurfer]);
+
+  const handleZoom = useCallback(
+    (newZoom: number) => {
+      if (wavesurfer) wavesurfer.zoom(newZoom);
+    },
+    [wavesurfer]
+  );
+
+  useEffect(() => {
+    if (zoom > 20) handleZoom(zoom);
+  }, [zoom]);
+
+  const changePitch = (e: number) => {
+    if (e > 0.07) wavesurfer!.setPlaybackRate(e, false);
+  };
 
   return (
-    <Stack style={{ border: "1px dotted white" }}>
-      <div
-        ref={containerRef}
-        className="waveFormCanvas"
-        style={{ border: "1px dotted white" }}
-      />
+    <Container>
+      <Group justify="space-between">
+        <Text>{activeRegion ? activeRegion.id : "no active"}</Text>
+        <Stack align="flex-start">
+          <Text>{follow ? "follow ON" : "follow OFF"}</Text>
+          <Text>{loop ? "Loop ON" : "Loop OFF"}</Text>
+        </Stack>
+      </Group>
 
-      <Stack style={{ border: "1px dotted white" }} justify="space-between">
-        <Group bg="red">
-          <Button onClick={onPlayClick}>{isPlaying ? "Pause" : "Play"}</Button>
-          <Text>Seconds played: {currentTime.toFixed(2)}</Text>
+      <Flex miw="100%">
+        <div
+          className="waveform-canvas"
+          ref={containerRef}
+          style={{
+            width: `calc(95% - ${rem(16)})`,
+            display: "flex",
+          }}
+        />
+        <PitchSlider changePitch={changePitch} />
+      </Flex>
+
+      <Group bg="red" p="md" justify="space-between">
+        <Group>
+          <ActionIcon
+            onMouseDown={() => playRegion(cuePoint!)}
+            onMouseUp={() => wavesurfer?.stop()}
+            bg="gray"
+          >
+            <IconCircle />
+          </ActionIcon>
+          <ActionIcon onMouseDown={onPlayClick} bg="gray">
+            {!isPlaying ? <IconPlayerPlayFilled /> : <IconPlayerPauseFilled />}
+          </ActionIcon>
         </Group>
+        <Text>Seconds played: {currentTime.toFixed(3)}</Text>
 
-        <Group bg="orange">
-          <IconZoomOut onClick={() => console.log("-")} />
+        <Group>
+          <ActionIcon onMouseDown={toggleFollow} bg={follow ? "" : "gray"}>
+            <IconSquareRoundedChevronRight />
+          </ActionIcon>
 
-          {/* <Slider
-            w="30%"
-            min={1}
-            max={1000}
-            color="pink"
-            value={minPxPerSec}
-            onChange={handleZoom}
-            size="lg"
-            showLabelOnHover={false}
-          /> */}
-
-          <IconZoomIn onClick={() => console.log("+")} />
-
-          <Group bg="blue">
-            <Checkbox label="Loop" />
-          </Group>
+          <ActionIcon onMouseDown={toggleLoop} bg={loop ? "" : "gray"}>
+            <IconRepeat />
+          </ActionIcon>
         </Group>
-      </Stack>
-    </Stack>
+      </Group>
+
+      <Group className="zoom-fader" bg="orange">
+        <IconZoomOut
+          onClick={() => {
+            if (zoom > 10) setZoom(zoom - 20);
+          }}
+        />
+        <Slider
+          w="30%"
+          min={8}
+          max={1000}
+          color="pink"
+          value={zoom}
+          onChange={setZoom}
+          size="lg"
+          showLabelOnHover={false}
+        />
+        <IconZoomIn onClick={() => setZoom(zoom + 20)} />
+      </Group>
+      <Group justify="flex-end" p="md">
+        <Text>Loop Points:</Text>
+        {savedRegions?.map((region: Region, i) => (
+          <Button
+            color={theme.colors.red[9 - i]}
+            key={region.id}
+            onClick={() => playRegion(region)}
+          >
+            {i}
+          </Button>
+        ))}
+      </Group>
+    </Container>
   );
 };
+
+export default WaveSurferPlayer;
