1921b793cba3b2192120ba2748b041054c22ff72
Joram3000 <joramkroon@live.nl>
2023-11-04 12:18:22 +0100

    add routes

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,9 @@ import ErrorPage from "./pages/error-page";
 import ContactPage from "./pages/contact-page";
 import AboutPage from "./pages/about-page";
 import PatternMakerPage from "./pages/patternmaker-page";
-import VideoPlayer from "./pages/videoplayer-page/Videoplayer";
+import VideoPlayer from "./pages/valkdigital-page/videoplayer/Videoplayer";
 import BeatBattleHomePage from "./pages/beatbattle-page/index";
-import TestPage from "./pages/test-pages";
+import TestPage from "./pages/test-page";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { createTheme, MantineProvider } from "@mantine/core";
@@ -17,6 +17,8 @@ import { ParallaxProvider } from "react-scroll-parallax";
 import ContestPage from "./pages/beatbattle-page/ContestPage";
 import "@mantine/carousel/styles.css";
 import "@mantine/core/styles.css";
+import ValkWorkIn from "./pages/valkdigital-page/ValkWorkIn";
+import ValkWorkItem from "./pages/valkdigital-page/ValkWorkItem";
 
 const router = createHashRouter([
   {
@@ -27,12 +29,14 @@ const router = createHashRouter([
       { path: "/", element: <HomePage /> },
       { path: "/*", element: <HomePage /> },
       { path: "/patternmaker", element: <PatternMakerPage /> },
-      { path: "/videoplayer", element: <VideoPlayer /> },
       { path: "/contact", element: <ContactPage /> },
       { path: "/about", element: <AboutPage /> },
       { path: "/test", element: <TestPage count={32} speed={8} /> },
       { path: "/beatbattle", element: <BeatBattleHomePage /> },
       { path: "/beatbattle/:id", element: <ContestPage /> },
+      { path: "/valkdigital", element: <ValkWorkIn /> },
+      { path: "/valkdigital/:id", element: <ValkWorkItem /> },
+      { path: "/valkdigital/narrowcasting", element: <VideoPlayer /> },
     ],
   },
 ]);
