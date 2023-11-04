6e1afe93fd64fd3f9575fbf4ab39fb6e236496df
Joram3000 <joramkroon@live.nl>
2023-11-04 12:17:39 +0100

    create nested lnks

--- src/routes/menuData.ts
+++ src/routes/menuData.ts
@@ -1,7 +1,15 @@
 export const menuData = [
   { link: "home", label: "menu.home" },
   { link: "patternmaker", label: "menu.patternMaker" },
-  { link: "videoplayer", label: "menu.videoPlayer" },
+  {
+    link: "valkdigital",
+    label: "menu.valkdigital",
+    links: [
+      { label: "nested1", link: "/hoe" },
+      { label: "nested2", link: "/die" },
+      { label: "nested3", link: "/ja" },
+    ],
+  },
   { link: "about", label: "menu.about" },
   { link: "contact", label: "menu.contact" },
   { link: "test", label: "menu.test" },
