ec85ea21177a5cd0c497c4a8b9d3e037fad93bf7
Joram3000 <joramkroon@live.nl>
2023-11-04 13:32:01 +0100

    add useles isDrawer to UI store

--- src/store/ui/actions.ts
+++ src/store/ui/actions.ts
@@ -7,3 +7,8 @@ export const DrawerToggle = (Payload: boolean) => ({
   type: "DRAWERTOGGLE",
   payload: Payload,
 });
+
+export const IsDrawer = (Payload: boolean) => ({
+  type: "ISDRAWER",
+  payload: Payload,
+});
