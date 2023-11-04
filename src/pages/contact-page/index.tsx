efcd3f223978c569af9501c06d16a7f855ba2142
Joram3000 <joramkroon@live.nl>
2023-11-04 12:18:38 +0100

    remove accordion

--- src/pages/contact-page/index.tsx
+++ src/pages/contact-page/index.tsx
@@ -1,48 +1,25 @@
-import { Title, Text, Image, Box, Accordion } from "@mantine/core";
+import { Title, Text, Image, Container, Group } from "@mantine/core";
+import Curriculum from "./curriculum";
+import { IconBrandInstagram, IconMail, IconPhone } from "@tabler/icons-react";
 
 export default function ContactPage() {
-  const groceries = [
-    {
-      emoji: "🍎",
-      value: "Apples",
-      description:
-        "Crisp and refreshing fruit. Apples are known for their versatility and nutritional benefits. They come in a variety of flavors and are great for snacking, baking, or adding to salads.",
-    },
-    {
-      emoji: "🍌",
-      value: "Bananas",
-      description:
-        "Naturally sweet and potassium-rich fruit. Bananas are a popular choice for their energy-boosting properties and can be enjoyed as a quick snack, added to smoothies, or used in baking.",
-    },
-    {
-      emoji: "🥦",
-      value: "Broccoli",
-      description:
-        "Nutrient-packed green vegetable. Broccoli is packed with vitamins, minerals, and fiber. It has a distinct flavor and can be enjoyed steamed, roasted, or added to stir-fries.",
-    },
-  ];
-
-  const items = groceries.map((item) => (
-    <Accordion.Item key={item.value} value={item.value}>
-      <Accordion.Control>{item.value}</Accordion.Control>
-      <Accordion.Panel>{item.description}</Accordion.Panel>
-    </Accordion.Item>
-  ));
-
   return (
-    <Box>
-      <Title p="md">ContactPage</Title>
-      <Text p="md">
-        Id labore cillum sunt. Magna pariatur pariatur anim aliqua fugiat elit
-        veniam velit aliqua. Reprehenderit officia ex pariatur Lorem aliquip
-        aliquip culpa consequat magna ex minim ea aliquip ullamco officia.
-        Dolore voluptate officia commodo elit sint ut in. Non dolore Lorem ea
-        non. Excepteur cupidatat veniam anim irure adipisicing aliquip aliqua
-        cillum fugiat.
-      </Text>
-      <Accordion p="md" variant="separated" defaultValue="huh">
-        {items}
-      </Accordion>
+    <Container>
+      <Title>Contact</Title>
+      <Group>
+        <IconPhone />
+        <Text>0618512084</Text>
+      </Group>
+      <Group>
+        <IconBrandInstagram />
+        <Text>Instagram @pracemusic</Text>
+      </Group>
+      <Group>
+        <IconMail />
+        <Text>joramkroon@live.nl</Text>
+      </Group>
+
+      <Curriculum />
 
       <Text p="md">
         Pariatur anim nulla ea incididunt sunt proident proident. Deserunt
@@ -81,6 +58,6 @@ export default function ContactPage() {
         in veniam exercitation Lorem duis est non nostrud occaecat qui. Eu quis
         nulla irure pariatur.
       </Text>
-    </Box>
+    </Container>
   );
 }
