02f8eee26a3c6da71174db2c4d68366da4ce9169
Joram3000 <joramkroon@live.nl>
2023-11-04 13:30:35 +0100

    update cv

--- src/pages/contact-page/curriculum.tsx
+++ src/pages/contact-page/curriculum.tsx
@@ -2,33 +2,33 @@ import { Accordion, Anchor, Group, Text } from "@mantine/core";
 import { lorem } from "../../helpers/helpers";
 
 const Curriculum: React.FC = () => {
-  const baantjes = [
+  const curriculumVitae = [
     {
       start: "2021",
       finish: "2023",
       title: "Front End Web Developer",
-      waar: "Valk Digital",
+      company: "Valk Digital",
       url: "https://www.valkdigital.nl/",
     },
     {
       start: "2020",
       finish: "heden",
       title: "Content Creator",
-      waar: "Kibbeling Media",
+      company: "Kibbeling Media",
       url: "https://www.kibbelingmedia.nl/",
     },
     {
       start: "2020",
       finish: "2022",
       title: "Muziekschool School of Beats",
-      waar: "School Of Beats",
+      company: "School Of Beats",
       url: "https://schoolofbeats.nl/",
     },
     {
       start: "2020",
       finish: "2022",
       title: "Educatieve Content creator",
-      waar: "Kunst Centraal",
+      company: "Kunst Centraal",
       url: "https://kunstcentraal.nl/",
     },
     {
@@ -41,7 +41,7 @@ const Curriculum: React.FC = () => {
       start: "2017",
       finish: "2020",
       title: "Artistiek coördinator en docent bij de Boempatsers",
-      waar: "Cultuurhuis Kanaleneiland",
+      company: "Cultuurhuis Kanaleneiland",
       url: "https://bokscultuurhuis.nl/",
     },
     {
@@ -49,14 +49,13 @@ const Curriculum: React.FC = () => {
       finish: "2018",
       title:
         "Oprichter Sound Safari – Muziekworkshops in gesloten inrichtingen",
-
       description: lorem.generateParagraphs(1),
     },
     {
       start: "2016",
       finish: "2018",
       title: "Projectmanager evenementen bij Safari & Bankra Bike Soundsystem",
-      waar: "Bankra Bike",
+      company: "Bankra Bike",
       url: "https://bankrabike.nl/",
     },
     {
@@ -68,13 +67,13 @@ const Curriculum: React.FC = () => {
       start: "2016",
       finish: "2018",
       title: "Docent Piano in Overvecht Utrecht",
-      waar: "UCK",
+      company: "UCK",
     },
     {
       start: "2014",
       finish: "2016",
       title: "Docent percussie bij project Muziekroute",
-      waar: "Muziekroute",
+      company: "Muziekroute",
       url: "https://www.muziekroute.nl/",
       description: lorem.generateParagraphs(1),
     },
@@ -82,7 +81,7 @@ const Curriculum: React.FC = () => {
       start: "2013",
       finish: "2016",
       title: "Docent Dj & Synthese/Sound Design bij",
-      waar: "Emstudios Academy Of Sound",
+      company: "Emstudios Academy Of Sound",
     },
     {
       start: "2011",
@@ -93,14 +92,14 @@ const Curriculum: React.FC = () => {
       start: "2011",
       finish: "2021",
       title: "Docent Dj en turntablism",
-      waar: "Dutch Dj Academy",
+      company: "Dutch Dj Academy",
       url: "https://www.dutchdjacademy.com/",
     },
     {
       start: "2011",
       finish: "2012",
       title: "Docent Dj en turntablism",
-      waar: "NPAC ROC",
+      company: "NPAC ROC",
     },
     {
       start: "2010",
@@ -125,7 +124,7 @@ const Curriculum: React.FC = () => {
     },
   ];
 
-  const items = baantjes.map((item, i) => (
+  const items = curriculumVitae.map((item, i) => (
     <Accordion.Item key={`${item.title} ${i}`} value={item.title}>
       <Accordion.Control chevron={item.description ? false : true}>
         <Group justify="space-between">
@@ -139,10 +138,10 @@ const Curriculum: React.FC = () => {
           <Text ta="right">
             {item.url ? (
               <Anchor href={item.url} target="_blank">
-                {item.waar}
+                {item.company}
               </Anchor>
             ) : (
-              item.waar
+              item.company
             )}
           </Text>
         </Group>
