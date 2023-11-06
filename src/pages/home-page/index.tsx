9a7e64ea208e284189438617cb09e5bcde75e62d
Joram3000 <joramkroon@live.nl>
2023-11-06 12:17:06 +0100

    improve copy

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -7,22 +7,25 @@ export default function HomePage() {
 
   return (
     <Container p={0}>
-      <Title p="md">HomePage</Title>
-      <Text p="md">{t("hello")}</Text>
+      <Title p="md">Joram Kroon Portfolio website</Title>
       <Text p="md">
         Op deze site staan verschillende projecten en stukken tech die ik
         gebruik en beheers. Deze website is bedoelt om inzicht te geven in wat
         ik allemaal kan. Het is echter altijd in progress dus ook al werkt er
         een boel, er wordt ook altijd aan gesleuteld.
       </Text>
+      <Text p="md">
+        Als developer hou ik van dynamische dingen maken. En van intuitieve
+        interfaces die uitnodigen om overal op te drukken.
+      </Text>
 
       <Title p="md" order={2}>
-        Tech die gebruikt is:
+        Gebruikte software:
       </Title>
       <TableSort />
       <Text p="md">
-        Het is echter altijd in progress dus ook al werkt er een boel, er wordt
-        ook altijd aan gesleuteld.
+        Deze website is bedoelt om inzicht te geven in wat ik allemaal kan
+        maken. Er wordt altijd wel aan gesleuteld ook.
       </Text>
     </Container>
   );
