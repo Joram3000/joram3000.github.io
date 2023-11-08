c2736454e99e3567efca847c0beb25d024023131
Joram3000 <joramkroon@live.nl>
2023-11-08 20:09:21 +0100

    clean up appshell

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,40 +1,20 @@
 import { Outlet } from "react-router-dom";
 import { useDisclosure } from "@mantine/hooks";
-import { AppShell, Button, Flex, Group, Text } from "@mantine/core";
+import { AppShell, Button, Flex } from "@mantine/core";
 import NavbarSimple from "../components/navbar/NavbarSimple";
 import { useEffect, useState } from "react";
 import HeaderSimple from "../components/header/HeaderSimple";
-// import { useTranslation } from "react-i18next";
-// import AsideComponent from "../components/aside/AsideComponent";
 import { MenuToggle } from "../store/ui/actions";
 import { useDispatch } from "react-redux";
-import { BrowserView, MobileView } from "react-device-detect";
-// import { IconQuestionMark } from "@tabler/icons-react";
-
-// import { IsDrawer } from "../../store/ui/actions";
-// const dispatch = useDispatch();
-// useEffect(() => {
-//   dispatch(IsDrawer(true));
-// }, []);
 
 export default function Root() {
   const dispatch = useDispatch();
-
-  // const getUIState = useSelector(getUIStateSelector);
-
-  // // const {
-  // //   i18n: { language: currentLanguage },
-  // // } = useTranslation();
-
-  // sessionStorage.setItem("language", currentLanguage);
-
   const [active, setActive] = useState("Home");
 
   const [navbarMobileOpened, { toggle: navbarMobileToggle }] =
     useDisclosure(false);
   const [navbarDesktopOpened, { toggle: navbarDesktopToggle }] =
     useDisclosure(true);
-  // const [asideOpened, { toggle: asideToggle }] = useDisclosure();
 
   useEffect(() => {
     setTimeout(() => {
@@ -59,14 +39,6 @@ export default function Root() {
           desktop: !navbarDesktopOpened,
         },
       }}
-      // aside={{
-      //   width: 300,
-      //   breakpoint: "md",
-      //   // collapsed: {
-      //   //   mobile: !asideOpened,
-      //   //   desktop: !asideOpened,
-      //   // },
-      // }}
     >
       <AppShell.Header>
         <HeaderSimple
@@ -83,35 +55,11 @@ export default function Root() {
         <Outlet />
       </AppShell.Main>
 
-      {/* <AppShell.Aside p="md">
-        <AsideComponent />
-      </AppShell.Aside> */}
-
       <AppShell.Footer>
         <Flex h={60} p="md" justify="space-between" align="center">
           <Button onClick={navbarDesktopToggle} visibleFrom="sm">
             {navbarDesktopOpened ? "hide menu" : "show menu"}
           </Button>
-
-          <BrowserView>
-            <Text>ComputerView</Text>
-          </BrowserView>
-          <MobileView>
-            <Text>MobileView</Text>
-          </MobileView>
-
-          {/* <Button onClick={() => dispatch(DrawerToggle(true))}>
-            Open drawer
-          </Button> */}
-          <Group></Group>
-          {/* <Button
-            variant="gradient"
-            onClick={() => dispatch(DrawerToggle(true))}
-            style={{ zIndex: 230 }}
-            m="sm"
-          >
-            <IconQuestionMark />
-          </Button> */}
         </Flex>
       </AppShell.Footer>
     </AppShell>
