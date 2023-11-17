fd18ef9d75748130669a6138e749ed322d0897e8
Joram3000 <joramkroon@live.nl>
2023-11-17 17:19:32 +0100

    update styling

--- src/pages/valkdigital-page/sounddesign/Sounddesign.tsx
+++ src/pages/valkdigital-page/sounddesign/Sounddesign.tsx
@@ -1,18 +1,18 @@
-import { Box, Container, Flex, Text, Title } from "@mantine/core";
+import { Box, Container, Flex, Stack, Text, Title } from "@mantine/core";
 import ReactPlayer from "react-player";
 import sdv2g from "../../../assets/video/sdv2g.webm";
 
 const SoundDesign = () => {
   return (
-    <Container>
+    <Container h="100%">
       <Title pt="md">Interface sound design</Title>
 
       <Flex
-        pt="md"
         direction={{ base: "column", sm: "row" }}
         align={{ base: "center", sm: "flex-start" }}
         justify="space-between"
         gap="md"
+        h="100%"
       >
         <Box>
           <ReactPlayer
@@ -23,29 +23,27 @@ const SoundDesign = () => {
           />
         </Box>
 
-        <Box>
-          <Title order={3}>Omschrijving</Title>
-          <Text>
-            Een van de leukste dingen die ik heb gedaan dit jaar is de sound
-            design ontwerpen voor een zelfscan kassa. Interface sound design is
-            een vak apart, en met mijn achtergrond als componist kan ik uit de
-            voeten met Ableton en synthese.
-          </Text>
-          <Text>
-            In de basis komt het neer op het nabouwen van de AH zelfscan kassa.
-            Het concept daar achter is dat je eigenlijk een pseudo-melodie die
-            zich ontvouwt aan de hand van de user interactions. Die tot een
-            bevredigend einde komt bij een succesvolle betaling
-          </Text>{" "}
+        <Stack justify="space-around">
+          <Box>
+            <Title order={3}>Omschrijving</Title>
+            <Text>
+              Voor de zelfscankassa's heb ik de interface sounds ontworpen.
+            </Text>
+            <Text>
+              In de basis komt het neer op het nabouwen van de AH zelfscan
+              kassa. De filosofie daarachter is dat je met elke handeling
+              audio-feedback krijgt, die als je een succesvolle betaling
+              verricht oplost in een consonant eind-akkoord.
+            </Text>
+          </Box>
           <Box>
             <ReactPlayer
               controls
               width="100%"
-              height="100%"
               url="https://res.cloudinary.com/dqqb0ldgk/video/upload/v1700234360/valktogo-sounddesign_rv1lfk.mov"
             />
           </Box>
-        </Box>
+        </Stack>
       </Flex>
     </Container>
   );
