757c1712f5fde7cf4465e7f6d0e3731c9e35ea41
Joram3000 <joramkroon@live.nl>
2023-11-17 16:48:21 +0100

    vulling voor sound design page

--- src/pages/valkdigital-page/sounddesign/Sounddesign.tsx
+++ src/pages/valkdigital-page/sounddesign/Sounddesign.tsx
@@ -1,7 +1,7 @@
-import { Container, Text, Title } from "@mantine/core";
+import { Box, Container, Flex, Paper, Text, Title } from "@mantine/core";
+import ReactPlayer from "react-player";
+import sdv2g from "../../../assets/video/sdv2g.webm";
 // import { useTranslation } from "react-i18next";
-
-//https://res.cloudinary.com/dqqb0ldgk/video/upload/v1700231430/valktogo-sounddesign_ign5uk.webm
 
 const SoundDesign = () => {
   // const { t } = useTranslation();
@@ -9,7 +9,51 @@ const SoundDesign = () => {
   return (
     <Container>
       <Title pt="md">Interface sound design</Title>
-      <Text py="md">Research & Sound voor de zelfscan kassa</Text>
+
+      <Container w="90%" h="100%">
+        <Flex
+          pt="md"
+          direction={{ base: "column", sm: "row" }}
+          align={{ base: "center", sm: "flex-start" }}
+          justify="space-between"
+          gap="md"
+        >
+          <Box>
+            <ReactPlayer
+              controls
+              width={1080 / 3}
+              height={1920 / 3}
+              url={sdv2g}
+            />
+          </Box>
+        
+          <Box>
+            <Title order={3}>Omschrijving</Title>
+            <Text>
+              Een van de leukste dingen die ik heb gedaan dit jaar is de sound
+              design ontwerpen voor een zelfscan kassa. Interface sound design
+              is een vak apart, en met mijn achtergrond als componist kan ik uit
+              de voeten met Ableton en synthese.
+            </Text>
+            <Text>
+              In de basis komt het neer op het nabouwen van de AH zelfscan
+              kassa. Het concept daar achter is dat je eigenlijk een
+              pseudo-melodie die zich ontvouwt aan de hand van de user
+              interactions. Die tot een bevredigend einde komt bij een
+              succesvolle betaling
+            </Text>
+          </Box>
+        </Flex>
+      </Container>
+
+      <Box>
+        <ReactPlayer
+          controls
+          width="100%"
+          height="100%"
+          url="https://res.cloudinary.com/dqqb0ldgk/video/upload/v1700234360/valktogo-sounddesign_rv1lfk.mov"
+        />
+      </Box>
     </Container>
   );
 };
