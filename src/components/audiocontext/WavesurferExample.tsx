f28af3c62ec1e9ae044756851a431e8c9c49ce85
Joram Kroon <joramkroon@live.nl>
2024-05-13 20:00:31 +0200

    move out audiodata try

--- src/components/audiocontext/WavesurferExample.tsx
+++ src/components/audiocontext/WavesurferExample.tsx
@@ -1,30 +1,30 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useCallback, useRef } from "react"
 import treingvbluesberber from "../../assets/music/treingvbluesberber.mp3"
 import BlankWaveSurfer from "../WaveSurferPlayer/BlankWaveSurfer"
 import P5WaveFormSketchWrapper from "../../pages/patternmaker-page/components/P5WaveFormSketchWrapper"
+
 const AudioEqualizer: React.FC = () => {
   const [audio] = useState(new Audio(treingvbluesberber))
-  const [audioContext, setAudioContext] = useState<AudioContext | null>(null)
-  const [filters, setFilters] = useState<BiquadFilterNode[]>([])
-  const [delay, setDelay] = useState<DelayNode>()
-  const [masterGain, setMasterGain] = useState<GainNode>()
-  const [delayGain, setDelayGain] = useState<GainNode>()
-  const initializeAudioContext = () => {
+  const [audioContextJ, setAudioContext] = useState<AudioContext | null>(null)
+  const filters = useRef<BiquadFilterNode[]>([])
+  const delay = useRef<DelayNode | null>(null)
+  const mediaNode = useRef<MediaElementAudioSourceNode | null>(null)
+  const analyser = useRef<AnalyserNode | null>(null)
+  const initializeAudioContext = async () => {
     const context = new AudioContext()
     setAudioContext(context)
   }
 
   useEffect(() => {
-    if (audioContext) {
+    if (audioContextJ) {
+      if (!mediaNode.current) {
+        mediaNode.current = audioContextJ.createMediaElementSource(audio)
+      }
       const eqBands = [32, 64, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]
-      const delayt = audioContext?.createDelay(4)
-      const masterGain = audioContext.createGain()
-      const delayGain = audioContext.createGain()
-      setDelay(delayt)
-      setMasterGain(masterGain)
-      setDelayGain(delayGain)
-      const createdFilters = eqBands.map((band) => {
-        const filter = audioContext.createBiquadFilter()
+      delay.current = audioContextJ.createDelay(4)
+      analyser.current = audioContextJ.createAnalyser()
+      filters.current = eqBands.map((band) => {
+        const filter = audioContextJ.createBiquadFilter()
         filter.type =
           band <= 32 ? "lowshelf" : band >= 16000 ? "highshelf" : "peaking"
         filter.gain.value = Math.random() * 40 - 20
@@ -33,60 +33,57 @@ const AudioEqualizer: React.FC = () => {
         return filter
       })
 
-      setFilters(createdFilters)
-    }
-  }, [audioContext])
+      if (mediaNode.current && filters.current.length > 0 && delay.current) {
+        mediaNode.current.connect(filters.current[0])
+        mediaNode.current.connect(analyser.current)
 
-  useEffect(() => {
-    if (
-      audio &&
-      audioContext &&
-      filters.length > 0 &&
-      delay &&
-      masterGain &&
-      delayGain
-    ) {
-      const mediaNode = audioContext.createMediaElementSource(audio)
-      mediaNode.connect(filters[0])
-      for (let i = 0; i < filters.length - 1; i++) {
-        filters[i].connect(filters[i + 1])
-      }
-      filters[filters.length - 1].connect(delay)
-      filters[filters.length - 1].connect(audioContext.destination)
-      delay.delayTime.setValueAtTime(0.5, audioContext.currentTime)
-
-      filters[filters.length - 1].connect(masterGain)
-      masterGain.connect(audioContext.destination)
-      delay.connect(delayGain)
-      delayGain.connect(audioContext.destination)
-      delay.connect(audioContext.destination)
-      return () => {
-        filters.forEach((filter) => filter.disconnect())
-        delay.disconnect()
-        masterGain.disconnect()
-        delayGain.disconnect()
+        for (let i = 0; i < filters.current.length - 1; i++) {
+          filters.current[i].connect(filters.current[i + 1])
+        }
+        filters.current[filters.current.length - 1].connect(delay.current)
+        filters.current[filters.current.length - 1].connect(
+          audioContextJ.destination,
+        )
+        delay.current.delayTime.setValueAtTime(0.5, audioContextJ.currentTime)
+        delay.current.connect(audioContextJ.destination)
+        return () => {
+          mediaNode.current?.disconnect()
+          filters.current.forEach((filter, i) => {
+            if (filters.current[i + 1]) {
+              filter.disconnect(filters.current[i + 1])
+            }
+          })
+          if (filters.current.length > 0 && delay.current) {
+            filters.current[filters.current.length - 1].disconnect(
+              delay.current,
+            )
+          }
+          delay.current?.disconnect()
+        }
       }
     }
-  }, [audio, audioContext, filters, delay, masterGain, delayGain])
+  }, [audio, audioContextJ])
 
-  const handleSliderChange = (index: number, value: string) => {
-    if (filters[index]) {
-      filters[index].gain.value = parseFloat(value)
+  const handleSliderChange = useCallback((index: number, value: string) => {
+    if (filters.current[index]) {
+      filters.current[index].gain.value = parseFloat(value)
     }
-  }
-
+  }, [])
   return (
     <div>
-      {filters.map((filter, index) => (
-        <input
-          key={index}
-          type="range"
-          min={-40}
-          max={40}
-          step={0.1}
-          defaultValue={filter.gain.value}
-          onChange={(e) => handleSliderChange(index, e.target.value)}
-        />
+      {filters.current.map((filter, index) => (
+        <>
+          <p>hoi</p>
+          <input
+            key={index}
+            type="range"
+            min={-40}
+            max={40}
+            step={0.1}
+            defaultValue={filter.gain.value}
+            onChange={(e) => handleSliderChange(index, e.target.value)}
+          />
+        </>
       ))}
 
       <button onClick={initializeAudioContext}>Initialize Audio Context</button>
