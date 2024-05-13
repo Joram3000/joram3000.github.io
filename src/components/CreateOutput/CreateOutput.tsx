8b0c3644bb0097a6c99c8270289920c2f8f4c9f3
Joram Kroon <joramkroon@live.nl>
2024-05-13 12:04:11 +0200

    create output

---
+++ src/components/CreateOutput/CreateOutput.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode, useEffect } from "react"
+import treingvbluesberber from "../../assets/music/treingvbluesberber.mp3"
+import P5WaveFormSketchWrapper from "../../pages/patternmaker-page/components/P5WaveFormSketchWrapper"
+import * as Tone from "tone"
+
+interface CreateOutputProps {
+  children: ReactNode
+}
+const channel = new Tone.Channel(-0.25, -1)
+console.log(channel)
+const output = new Tone.Volume(-12).toDestination()
+const lpFilter = new Tone.Filter(8000, "lowpass", -48).connect(output)
+const hpFilter = new Tone.Filter(0, "highpass").connect(lpFilter)
+
+function CreateOutput({ children }: CreateOutputProps) {
+  console.log("hoi")
+
+  return <div>{children}</div>
+}
+
+export default CreateOutput
