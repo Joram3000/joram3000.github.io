94362a4ae5d14ec862f709888669603b4789c582
Joram Kroon <joramkroon@live.nl>
2024-05-13 21:18:52 +0200

    delete filters for simplicty

--- src/components/audiocontext/WavesurferExample.tsx
+++ src/components/audiocontext/WavesurferExample.tsx
@@ -1,89 +1,53 @@
-import React, { useEffect, useState, useCallback, useRef } from "react"
-import treingvbluesberber from "../../assets/music/treingvbluesberber.mp3"
-import BlankWaveSurfer from "../WaveSurferPlayer/BlankWaveSurfer"
+import React, { useEffect, useState, useRef } from "react" // Importing necessary modules from React
+import treingvbluesberber from "../../assets/music/treingvbluesberber.mp3" // Importing the audio file
+import BlankWaveSurfer from "../WaveSurferPlayer/BlankWaveSurfer" // Importing a WaveSurfer component
 import { Slider } from "@mantine/core"
 
 const AudioEqualizer: React.FC = () => {
-  const [audio] = useState(new Audio(treingvbluesberber))
-  const [audioContextJ, setAudioContext] = useState<AudioContext | null>(
-    new AudioContext(),
+  const [audio] = useState(new Audio(treingvbluesberber)) // State hook for audio
+
+  const [audioContext] = useState<AudioContext | null>(
+    new AudioContext(), // State hook for audio context
   )
-  // const feedbackGainNode = useRef<GainNode>(audioContextJ.createGain())
-  // console.log(feedbackGainNode.current)
-  const filters = useRef<BiquadFilterNode[]>([])
-  const [filtersReady, setFiltersReady] = useState(false)
-  const delay = useRef<DelayNode | null>(null)
-  const mediaNode = useRef<MediaElementAudioSourceNode | null>(null)
+  const mediaNode = useRef<MediaElementAudioSourceNode | null>(null) // Ref for media node
+  const lowPassFilter = useRef<BiquadFilterNode | null>(null) // Ref for low-pass filter
+  const [lowpassFilterFrequency, setLowpassFilterFrequency] = useState(300) // State hook for lowpass filter frequency
 
   useEffect(() => {
-    const eqBands = [32, 64, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]
+    if (audioContext) {
+      // Create a low-pass filter
+      lowPassFilter.current = audioContext.createBiquadFilter()
+      lowPassFilter.current.type = "lowpass" // Set filter type to lowpass
 
-    if (audioContextJ) {
       if (!mediaNode.current) {
-        mediaNode.current = audioContextJ.createMediaElementSource(audio)
+        mediaNode.current = audioContext.createMediaElementSource(audio)
       }
 
-      delay.current = audioContextJ.createDelay(3)
-      filters.current = eqBands.map((band) => {
-        const filter = audioContextJ.createBiquadFilter()
-        filter.type =
-          band <= 32 ? "lowshelf" : band >= 16000 ? "highshelf" : "peaking"
-        filter.gain.value = Math.random() * 40 - 20
-        filter.Q.value = 1
-        filter.frequency.value = band
-        return filter
-      })
-
-      if (mediaNode.current && filters.current.length > 0 && delay.current) {
-        mediaNode.current.connect(filters.current[0])
-        setFiltersReady(true)
-
-        for (let i = 0; i < filters.current.length - 1; i++) {
-          filters.current[i].connect(filters.current[i + 1])
-        }
-        filters.current[filters.current.length - 1].connect(delay.current)
-        filters.current[filters.current.length - 1].connect(
-          audioContextJ.destination,
-        )
-        delay.current.delayTime.setValueAtTime(0.1, audioContextJ.currentTime)
-        delay.current.connect(audioContextJ.destination)
+      if (mediaNode.current) {
+        mediaNode.current.connect(lowPassFilter.current)
+        lowPassFilter.current.connect(audioContext.destination)
         return () => {
           mediaNode.current?.disconnect()
-          filters.current.forEach((filter, i) => {
-            if (filters.current[i + 1]) {
-              filter.disconnect(filters.current[i + 1])
-            }
-          })
-          if (filters.current.length > 0 && delay.current) {
-            filters.current[filters.current.length - 1].disconnect(
-              delay.current,
-            )
-          }
-          delay.current?.disconnect()
         }
       }
     }
-  }, [audio, audioContextJ])
+  }, [audio, audioContext])
 
-  const handleSliderChange = useCallback((index: number, value: string) => {
-    if (filters.current[index]) {
-      filters.current[index].gain.value = parseFloat(value)
+  const handleFaderChange = (e: number) => {
+    if (lowPassFilter.current) {
+      lowPassFilter.current.frequency.value = e
     }
-  }, [])
+  }
 
   return (
     <div>
-      {filtersReady &&
-        filters.current.map((filter, index) => (
-          <Slider
-            key={index}
-            min={-40}
-            max={40}
-            step={0.1}
-            defaultValue={filter.gain.value}
-            onChange={(e) => handleSliderChange(index, e.toString())}
-          />
-        ))}
+      <Slider
+        min={0}
+        max={4000}
+        step={0.1}
+        defaultValue={lowpassFilterFrequency}
+        onChange={(e) => handleFaderChange(e)}
+      />
       <BlankWaveSurfer
         media={audio}
         dragToSeek
