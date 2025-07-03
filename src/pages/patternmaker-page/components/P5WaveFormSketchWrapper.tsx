c51b4b8b0922ff9ccc2379529fe42b6ac509d13c
Joram Kroon <joramkroon@live.nl>
2025-07-03 19:53:21 +0200

    update for p5

--- src/pages/patternmaker-page/components/P5WaveFormSketchWrapper.tsx
+++ src/pages/patternmaker-page/components/P5WaveFormSketchWrapper.tsx
@@ -1,67 +1,74 @@
-// @ts-ignore
-import { ReactP5Wrapper, Sketch, SketchProps } from "@p5-wrapper/react";
-import * as Tone from "tone";
+import * as React from "react"
+import {
+  ReactP5Wrapper,
+  Sketch,
+  SketchProps,
+  P5CanvasInstance,
+} from "@p5-wrapper/react"
+import * as Tone from "tone"
 
 type waveformSketchProps = SketchProps & {
-  color: string;
-};
+  color: string
+}
 
-let meter;
-let analyser: Tone.Analyser;
-const playing = true;
-let waveColor: string;
+let meter: Tone.Meter
+let analyser: Tone.Analyser
+const playing = true
+let waveColor: string
 
-const waveformSketch: Sketch<waveformSketchProps> = (p: any) => {
-  meter = new Tone.Meter();
-  Tone.Destination.connect(meter);
-  p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight);
-    p.fill(0);
+const waveformSketch: Sketch<waveformSketchProps> = (
+  p5: P5CanvasInstance<waveformSketchProps>,
+) => {
+  meter = new Tone.Meter()
+  Tone.Destination.connect(meter)
 
-    analyser = new Tone.Analyser("waveform", 512);
-    Tone.Destination.connect(analyser);
-  };
+  p5.setup = () => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight)
+    p5.fill(0)
 
-  p.updateWithProps = (props: waveformSketchProps) => {
+    analyser = new Tone.Analyser("waveform", 512)
+    Tone.Destination.connect(analyser)
+  }
+
+  p5.updateWithProps = (props: waveformSketchProps) => {
     if (props.color) {
-      waveColor = props.color;
+      waveColor = props.color
     }
-  };
+  }
 
-  p.windowResized = () => {
-    p.resizeCanvas(p.windowWidth, p.windowHeight);
-  };
+  p5.windowResized = () => {
+    p5.resizeCanvas(p5.windowWidth, p5.windowHeight)
+  }
 
-  p.draw = () => {
-    p.clear(0, 0, 0, 0);
-    const dim = Math.min(p.width, p.height);
-    p.strokeWeight(dim * 0.003);
-    p.stroke(waveColor);
-    p.noFill();
+  p5.draw = () => {
+    p5.clear(0, 0, 0, 0)
+    const dim = Math.min(p5.width, p5.height)
+    p5.strokeWeight(dim * 0.003)
+    p5.stroke(waveColor)
+    p5.noFill()
     if (playing) {
-      const values = analyser.getValue() as Float32Array;
+      const values = analyser.getValue() as Float32Array
 
-      p.beginShape();
+      p5.beginShape()
       for (let i = 0; i < values.length; i++) {
-        const amplitude = values[i] / 2;
-        const x = p.map(i, 0, values.length - 1, 0, p.width);
-        const y = p.height / 2;
-        p.vertex(x, y + amplitude * (p.height / 2));
+        const amplitude = values[i] / 2
+        const x = p5.map(i, 0, values.length - 1, 0, p5.width)
+        const y = p5.height / 2
+        p5.vertex(x, y + amplitude * (p5.height / 2))
       }
-      p.endShape();
+      p5.endShape()
     }
-    p.background(0, 0, 0, 0);
-  };
-};
+  }
+}
 
 interface P5WaveFormSketchWrapperProps {
-  colorValue: string;
+  colorValue: string
 }
 
 const P5WaveFormSketchWrapper: React.FC<P5WaveFormSketchWrapperProps> = ({
   colorValue,
 }) => {
-  return <ReactP5Wrapper sketch={waveformSketch} color={colorValue} />;
-};
+  return <ReactP5Wrapper sketch={waveformSketch} color={colorValue} />
+}
 
-export default P5WaveFormSketchWrapper;
+export default P5WaveFormSketchWrapper
